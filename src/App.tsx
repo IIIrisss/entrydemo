064deb063ac5cf2b28294826e1aedfd665c7cce4
IIIrisss <wy22302010061@163.com>
2023-10-02 17:35:04 +0800

    version 0.2
    add switchToGeorli()

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,50 @@ import { ethers } from 'ethers';
 function App() {
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  
+  var provider = new ethers.BrowserProvider(window.ethereum);
+
   const ConnectMetamask = async () => {
     console.log(`Button 1 clicked with value: ${input1}`);
-    if(provider==null)throw Error(`Provider is null`);
-    
+    if (provider == null) throw Error(`Provider is null`);
     else {
-      const chainId = await provider.getNetwork();
-       console.log(`chainid: ${chainId}`)
-     
-       console.log(`Provider is not null`);
-     const accounts = await provider.send("eth_requestAccounts", []);
-     const account = accounts[0];
-     
-     if(accounts.length === 0) {
-      throw Error('未登录任何账户');
-    }
-     else {
-      console.log(`钱包地址: ${account}`);
-     const signer = await provider.getSigner();
-     const balance = await provider.getBalance(signer.getAddress());
-     console.log(`以太坊余额： ${ethers.formatUnits(balance)}`);
-     }}
-    
-  }
-  const TransferButton = async() => {
-    const signer = await provider.getSigner();
-    const tx = {
-      to: input2,
-      value: ethers.parseEther(input1),
-    }
-    console.log('开始转账');
-    const receipt = await signer.sendTransaction(tx);
-    await receipt.wait();
-    console.log('完成转账');
-    
-  };
+      console.log(`Provider is not null`);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const account = accounts[0];
 
+      if (accounts.length === 0) {
+        throw Error('未登录任何账户');
+      }
+      else {
+        console.log(`钱包地址: ${account}`);
+        const signer = await provider.getSigner();
+        const balance = await provider.getBalance(signer.getAddress());
+        console.log(`以太坊余额： ${ethers.formatUnits(balance)}`);
+      }
+    }
+
+  }
+  const TransferButton = async () => {
+     let chainId= (await provider.getNetwork()).chainId;
+    if (Number(chainId) != 5) {
+      console.log('链ID不是以太坊测试网Goerli链');
+      console.log(chainId);
+      switchToGeorli();
+    } else {
+      const signer = await provider.getSigner();
+      const tx = {
+        to: input2,
+        value: ethers.parseEther(input1),
+      }
+      console.log('开始转账');
+      const receipt = await signer.sendTransaction(tx);
+      await receipt.wait();
+      console.log('完成转账');
+
+    }
+
+
+
+  }
   return (
     <div className="App">
       <img src='./logo192.png' className="App-logo" alt="logo" />
@@ -69,6 +75,15 @@ function App() {
   );
 }
 
-
+async function switchToGeorli() {
+  try {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x5' }], // 0x5 表示 Ropsten 测试网络
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default App;
